be610d11ec08fd8d78481355034e0be3b90d664d
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-07-21 11:06:15 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Create a new <div> element
+let div = document.createElement("div");
+
+// Set the 'id' attribute of the <div> to 'browser-info'
+div.id = 'browser-info';
+
+// Append the <div> to the <body> of the current document
+document.body.append(div);
+
+// Set the innerHTML of the <div> to display browser information
+div.innerHTML = "You are using " + navigator.appName + " version " + navigator.appVersion;
